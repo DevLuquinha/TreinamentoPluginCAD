bd4baaa87890714e0aec0f4d6d477d0f9cf83607
LucasEmmanuel_DEV <158118046+luquinhas737@users.noreply.github.com>
2024-11-04 15:59:06 -0300

    Adicionar arquivos de projeto.

---
+++ TreinamentoPluginCAD/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+namespace TreinamentoPluginCAD
+{
+    public class Main
+    {
+        [CommandMethod("OLAMUNDO")]
+        public void MostrarMensagem()
+        {
+            
+
+        }
+    }
+}
